3a1bc11518b32c8a389a4924b9fcdcee80d2ab2f
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2020-01-25 03:21:58 +0100

    Posting on reddit

---
+++ script.js
@@ -0,0 +1,23 @@
+// START CLOCK SCRIPT
+
+Number.prototype.pad = function(n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
+
+function updateClock() {
+  var now = new Date();
+  var min = now.getMinutes(),
+    hou = now.getHours();
+  var tags = ["h", "m"],
+    corr = [hou.pad(2), min.pad(2)];
+  for (var i = 0; i < tags.length; i++)
+    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
+}
+
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1000);
+}
+
+// END CLOCK SCRIPT
