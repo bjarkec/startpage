28ba634730b8296e255761e8f00325903c5f2940
Tobias.Schoch <43953160+Tobias-Schoch@users.noreply.github.com>
2020-01-26 00:02:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -16,9 +16,15 @@ function initClock() {
   window.setInterval("updateClock()", 1000);
 }
 
+document.body.style.background = localStorage.getItem("background");
+
 $(document).keyup(function(e) {
      if (e.key === "Escape") { // escape key maps to keycode `27`
         var hex = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
 		document.body.style.background = hex;
+		localStorage.setItem('background', hex);
+    } else if (e.key === "Insert") { 
+		document.body.style.background = "#202124";
+		localStorage.setItem('background', "#202124");
     }
 });
